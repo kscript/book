3f4687c8b04a1e01fd8a012cdb335a564fb12f9e
kscript <1258@live.cn>
2021-10-29 18:15:17 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/scss/base.scss'
+if (process.env.NODE_ENV === 'development') {
+    require('github-markdown.css')
+    require('highlight.js/styles/atom-one-dark-reasonable.css')
+}
 
 createApp(App).use(store).use(router).mount('#app')
