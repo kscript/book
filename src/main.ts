f1ccd19219d91892b97a8a66e065d3e82eacc559
kscript <1258@live.cn>
2021-10-29 17:11:23 +0800

    样式文件与项目代码分离

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/scss/base.scss'
-import 'github-markdown-css'
-import 'highlight.js/styles/atom-one-dark-reasonable.css'
 
 createApp(App).use(store).use(router).mount('#app')
