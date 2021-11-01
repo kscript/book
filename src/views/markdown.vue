8793db68fae7606ba16ed4a4381ba52350060ea3
kscript <1258@live.cn>
2021-11-01 11:22:32 +0800

    不再直接将文件名作为标题

--- src/views/markdown.vue
+++ src/views/markdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="article-container">
-    <h1>{{id}}</h1>
+    <!-- <h1>{{id}}</h1> -->
     <vue-markdown :md="md" :html="html" :id="id"></vue-markdown>
   </div>
 </template>
