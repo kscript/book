1febc468af769b78dd6544b1189ad2c2f61fd34f
kscript <1258@live.cn>
2021-11-02 10:28:23 +0800

    保持资源加载顺序

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,38 @@
 import { loadScript, loadStyle } from './utils'
 import { config } from './config'
 
-const load = (list: string[], callback: (item: string, resolve: (value: unknown) => void, reject: (value: unknown) => void) => void) => {
-  if (Array.isArray(list) && list.length) {
-    return Promise.all(
-      list.map(item => {
-        return new Promise((resolve, reject) => {
-          callback(item, resolve, reject)
+const load = async (queue: string[], callback: (item: string, resolve: (value: unknown) => void, reject: (value: unknown) => void) => void, keep: boolean) => {
+  if (Array.isArray(queue) && queue.length) {
+    if (keep) {
+      let stack: Promise<unknown> = Promise.resolve()
+      queue.forEach(item => {
+        stack = stack.then(() => {
+          return new Promise((resolve: (value: unknown) => void, reject: (value: unknown) => void) => {
+            callback(item, resolve, reject)
+          })
         })
       })
-    )
+      await stack
+    } else {
+      await Promise.all(
+        queue.map(item => {
+          return new Promise((resolve, reject) => {
+            callback(item, resolve, reject)
+          })
+        })
+      )
+    }
   }
 }
+
 Promise.all([
   load(config.scripts, (item, resolve) => {
     loadScript(item, () => resolve(true))
-  }),
+  }, true),
   load(config.styles, (item, resolve) => {
     loadStyle(item)
     resolve(true)
-  })
+  }, true)
 ]).then(() => {
   const { loadApp } = require('./app')
   loadApp()
